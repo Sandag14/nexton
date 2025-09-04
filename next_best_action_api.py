2ba3700e51818adffea3575e1893739c71b7c395
Sandagdorj <58383936+Sandag14@users.noreply.github.com>
2025-09-04 14:48:30 +0800

    [FIX] cors

--- next_best_action_api.py
+++ next_best_action_api.py
@@ -8,7 +8,17 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"], methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"])
+allowed_origins = [
+    "http://localhost:3000",       # Dev Next.js
+    "https://chatbot.tavanbogd.com"    # Production Next.js
+]
+CORS(
+    app,
+    resources={r"/api/*": {"origins": allowed_origins}},
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"],
+    methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"]
+)
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY", "")
 
